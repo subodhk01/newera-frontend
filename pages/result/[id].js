bd167049147c97ceeed5b965a20df1e30068c104
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-12-15 17:14:54 +0530

    Added charts on results page

--- pages/result/[id].js
+++ pages/result/[id].js
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router'
 import AuthHOC from '../../components/AuthHOC'
 import SideBarLayout from '../../components/UI/WithSideBar'
 
+import { Bar } from "react-chartjs-2";
+
 // import dynamic from 'next/dynamic'
 
 // const axiosInstance = dynamic(() =>
@@ -19,6 +21,8 @@ export default function Test(props){
     const [ loading, setLoading ] = React.useState(true)
     const [ result, setResult ] = React.useState()
     const [ test, setTest ] = React.useState()
+    const [ data1, setData1 ] = React.useState()
+    const [ data2, setData2 ] = React.useState()
     
     React.useEffect(() => {
         if(id){
@@ -27,6 +31,42 @@ export default function Test(props){
                 console.log("result: ", response.data)
                 setResult(response.data)
                 setTest(response.data && response.data.test)
+                const chartData1 = {
+                    labels: ["You", "Average", "Highest"],
+                    datasets: [
+                        {
+                            label: "Marks",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: "butt",
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: "miter",
+                            data: [response.data.marks.total, response.data.test.stats.average.overall, response.data.test.stats.highest.overall ]
+                        }
+                    ]
+                }
+                const chartData2 = {
+                    labels: response.data.test.sections,
+                    datasets: [
+                        {
+                            label: "Marks",
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: "butt",
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: "miter",
+                            data: response.data.marks.section_wise
+                        }
+                    ]
+                }
+                setData1(chartData1)
+                setData2(chartData2)
                 setLoading(false)
             }).catch((error) => {
                 console.log(error)
@@ -41,7 +81,7 @@ export default function Test(props){
                     {!loading &&
                         <>
                             <div>
-                                <h2>Result Analysis</h2>
+                                <h2>Result Analysis {result.practice && <span className="text-warning">Practice Attempt</span>}</h2>
                             </div>
                             <div className="py-3 d-flex align-items-center justify-content-end">
                                 <Link href={`/test/review/${id}`}>
@@ -65,28 +105,60 @@ export default function Test(props){
                                     <h2>Marks Obtained: {result.marks && result.marks.total}/{result.marks && result.marks.max_marks && result.marks.max_marks[result.marks.max_marks.length - 1]}</h2>
                                 </div>
                             </div>
-                            <div className="w-75 mx-auto">
-                                <h6>Ranks</h6>
-                                <table className="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>Section</th>
-                                            <th>Ranks</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {test.sections && test.sections.map((section, index) =>
-                                            <tr key={`sectionRow-${index}`}>
-                                                <td>{section}</td>
-                                                <td>{result && result.ranks && result.ranks.section_wise && result.ranks.section_wise[index]}</td>
-                                            </tr>
-                                        )}
-                                        <tr>
-                                            <td><strong>Overall</strong></td>
-                                            <td>{result && result.ranks && result.ranks.overall}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                            <div className="d-flex flex-wrap align-items-center justify-content-center py-4">
+                                {result.ranks && 
+                                    <div className="">
+                                        <h6>Ranks</h6>
+                                        <table className="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>Section</th>
+                                                    <th>Ranks</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {test.sections && test.sections.map((section, index) =>
+                                                    <tr key={`sectionRow-${index}`}>
+                                                        <td>{section}</td>
+                                                        <td>{result && result.ranks && result.ranks.section_wise && result.ranks.section_wise[index]}</td>
+                                                    </tr>
+                                                )}
+                                                <tr>
+                                                    <td><strong>Overall</strong></td>
+                                                    <td>{result && result.ranks && result.ranks.overall}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                }
+                                <div className="col-12 col-lg-auto">
+                                    <Bar
+                                        data={data1}
+                                        options={{
+                                            scales: {
+                                                yAxes: [{
+                                                    ticks: {
+                                                        suggestedMin: 0,
+                                                        suggestedMax: result.marks.max_marks[result.marks.max_marks.length - 1]
+                                                    }
+                                                }]
+                                            },
+                                            // maintainAspectRatio: false,
+                                            // aspectRatio: 0.5
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-12 col-lg-auto">
+                                    <Bar
+                                        data={data2}
+                                        // height="100%"
+                                        // width="100%"
+                                        // options={{
+                                        //     maintainAspectRatio: false,
+                                        //     aspectRatio: 0.5
+                                        // }}
+                                    />
+                                </div>
                             </div>
                             <div className="pt-3">
                                 <div className="row no-gutters">
