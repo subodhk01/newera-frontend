45fc75f9c252300be4ffdbbbd76fcad7fe4ff756
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-12-25 01:30:03 +0530

    Changed prod url

--- utils/axios.js
+++ utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import cookie from 'react-cookies'
 
 const debug = 0
-const prod = 0
+const prod = 1
 export const baseURL = debug ? "http://localhost:8000/" : 'https://newera-backend.herokuapp.com/'
 export const prodURL = "http://api.neweraonlinecoaching.com/"
 //const baseURL = debug ? "http://localhost:8000/" : 'http://materate-math-a-hack-results.ap-south-1.elasticbeanstalk.com/'
