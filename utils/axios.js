6a7c09bfd5bdb24add8c0bb61c1a0246c265b378
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-12-25 21:41:15 +0530

    Added prod url for everything

--- utils/axios.js
+++ utils/axios.js
@@ -3,8 +3,8 @@ import cookie from 'react-cookies'
 
 const debug = 0
 const prod = 1
-export const baseURL = debug ? "http://localhost:8000/" : 'https://newera-backend.herokuapp.com/'
 export const prodURL = "https://api.neweraonlinecoaching.com/"
+export const baseURL = prod ? prodURL : (debug ? "http://localhost:8000/" : 'https://newera-backend.herokuapp.com/')
 //const baseURL = debug ? "http://localhost:8000/" : 'http://materate-math-a-hack-results.ap-south-1.elasticbeanstalk.com/'
 
 const axiosInstance = axios.create({
